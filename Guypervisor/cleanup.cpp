a715f855494dd084760b1e856de31fd1bc21a07b
1byte <sudaiguy1@gmail.com>
2020-11-21 14:59:47 +0200

    Added tag

--- Guypervisor/cleanup.cpp
+++ Guypervisor/cleanup.cpp
@@ -2,23 +2,21 @@
 
 #include <wdm.h>
 
+constexpr ULONG kGuypervisorTag = 'GuyP';
+
 void* operator new(size_t count)
 {
-    // Allocate new device object
-    return ExAllocatePoolWithTag(NonPagedPoolExecute,
-        count,
-        'guyp');
+    // Allocate new device object in the nonpaged-pool
+    return ExAllocatePoolWithTag(NonPagedPoolExecute, count, kGuypervisorTag);
 }
 
 void operator delete (void* p) {
     // Free device object
-    ExFreePoolWithTag(p,
-        'guyp');
+    ExFreePoolWithTag(p, kGuypervisorTag);
 }
 
 void operator delete (void* p, unsigned __int64 size) {
     UNREFERENCED_PARAMETER(size);
     // Free device object
-    ExFreePoolWithTag(p,
-        'guyp');
+    ExFreePoolWithTag(p, kGuypervisorTag);
 }
