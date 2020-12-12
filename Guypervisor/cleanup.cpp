7948c0dff779c68528db4044ce684f893b880e51
1byte <sudaiguy1@gmail.com>
2020-12-13 00:59:03 +0200

    Added custom error codes and cleanup stuff

--- Guypervisor/cleanup.cpp
+++ Guypervisor/cleanup.cpp
@@ -2,12 +2,22 @@
 
 #include <wdm.h>
 
+#include "print.h"
+
 constexpr ULONG kGuypervisorTag = 'GuyP';
 
 void* operator new(size_t count)
 {
     // Allocate new device object in the nonpaged-pool
-    return ExAllocatePoolWithTag(NonPagedPoolExecute, count, kGuypervisorTag);
+    void* ret_addr = ExAllocatePoolWithTag(NonPagedPoolExecute, count, kGuypervisorTag);
+    if (ret_addr == nullptr) {
+        MDbgPrint("Couldn't allocate %ld bytes\n", count);
+        // TODO: Handle bad memory allocation
+        goto cleanup;
+    }
+
+cleanup:
+    return ret_addr;
 }
 
 void operator delete (void* p) {
