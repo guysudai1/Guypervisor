f2f972bfeee73dc60c0b8826fb3a139e52ef1582
1byte <sudaiguy1@gmail.com>
2020-12-25 22:49:59 +0200

    Added Get selector functions

---
+++ Guypervisor/vmcs_init_helpers.asm
@@ -0,0 +1,73 @@
+.CODE 
+public GetEsSelector
+public GetCsSelector
+public GetSsSelector
+public GetDsSelector
+public GetFsSelector
+public GetGsSelector
+public GetLdtrSelector
+public GetTrSelector
+public __read_rsp
+public __read_esp
+
+GetEsSelector PROC
+; Returns es selector register
+mov ax, es
+ret
+GetEsSelector ENDP
+
+GetCsSelector PROC
+; Returns cs selector register
+mov ax, cs
+ret
+GetCsSelector ENDP
+
+GetSsSelector PROC
+; Returns ss selector register
+mov ax, ss
+ret
+GetSsSelector ENDP
+
+GetDsSelector PROC
+; Returns ds selector register
+mov ax, ds
+ret
+GetDsSelector ENDP
+
+GetFsSelector PROC
+; Returns fs selector register
+mov ax, fs
+ret
+GetFsSelector ENDP
+
+GetGsSelector PROC
+; Returns gs selector register
+mov ax, gs
+ret
+GetGsSelector ENDP
+
+GetLdtrSelector PROC
+; Returns ldtr selector register
+lldt ax
+ret
+GetLdtrSelector ENDP
+
+GetTrSelector PROC
+; Returns tr selector register
+ltr ax
+ret
+GetTrSelector ENDP
+
+__read_rsp PROC
+    mov rax, rsp
+    add rax, 8
+    ret
+__read_rsp ENDP
+    
+__read_esp PROC
+    mov eax, esp
+    add eax, 4
+    ret
+__read_esp ENDP
+
+END
